ac92f60515137a87e49f63d68fdb81ad9c3b07bf
clouwud <dakshgajbhiye902@gmail.com>
2025-09-21 04:57:43 +0530

    init

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,31 @@
+return {
+	{
+		"akinsho/toggleterm.nvim",
+		version = "*",
+		config = function()
+			require("toggleterm").setup({
+				size = 20,
+				open_mapping = [[`]], -- or <Space>t if this doesn't work
+				hide_numbers = true,
+				shade_filetypes = {},
+				shade_terminals = true,
+				shading_factor = 2,
+				start_in_insert = true,
+				insert_mappings = true,
+				terminal_mappings = true,
+				persist_size = true,
+				direction = "float",
+				close_on_exit = true,
+				shell = vim.o.shell,
+				float_opts = {
+					border = "curved",
+					winblend = 3,
+					highlights = {
+						border = "Normal",
+						background = "Normal",
+					},
+				},
+			})
+		end,
+	},
+}

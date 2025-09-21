f1bd1bdbbba4c0fd61b03e254e1f054b8fdc99d0
clouwud <dakshgajbhiye902@gmail.com>
2025-09-21 20:42:12 +0530

    init

--- lua/plugins/indent.lua
+++ lua/plugins/indent.lua
@@ -1,24 +1,26 @@
 return {
-  { -- Add indentation guides even on blank lines
-    "lukas-reineke/indent-blankline.nvim",
-    main = "ibl",
-    opts = {
-      indent = {
-        char = "▏",
-      },
-      exclude = {
-        buftypes = { "terminal", "nofile" },
-        filetypes = {
-          "help",
-          "startify",
-          "dashboard",
-          "lazy",
-          "neogitstatus",
-          "NvimTree",
-          "Trouble",
-        },
-      },
-    },
-  },
+	{
+		"NMAC427/guess-indent.nvim",
+	},
+	{ -- Add indentation guides even on blank lines
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		opts = {
+			indent = {
+				char = "▏",
+			},
+			exclude = {
+				buftypes = { "terminal", "nofile" },
+				filetypes = {
+					"help",
+					"startify",
+					"dashboard",
+					"lazy",
+					"neogitstatus",
+					"NvimTree",
+					"Trouble",
+				},
+			},
+		},
+	},
 }
-

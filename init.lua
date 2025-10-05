96d396af00920ade9a02c40c9c93493d06ef4df4
clouwud <dakshgajbhiye902@gmail.com>
2025-10-05 14:50:41 +0530

    init

--- init.lua
+++ init.lua
@@ -3,18 +3,18 @@ require("lazynvim")
 require("keymaps")
 require("autocmds")
 
-vim.o.clipboard = 'unnamedplus'
+vim.o.clipboard = "unnamedplus"
 vim.o.background = "dark" -- or "light" for light mode
 
 -- Use spaces instead of tabs
-vim.o.expandtab = true     -- Use spaces instead of tabs
-vim.o.tabstop = 2          -- A tab character represents 4 spaces
-vim.o.shiftwidth = 2       -- Indentation level is 4 spaces
-vim.o.softtabstop = 2      -- When pressing <Tab> or <BS>, it behaves like 4 spaces
+vim.o.expandtab = true -- Use spaces instead of tabs
+vim.o.tabstop = 4 -- A tab character represents 4 spaces
+vim.o.shiftwidth = 4 -- Indentation level is 4 spaces
+vim.o.softtabstop = 4 -- When pressing <Tab> or <BS>, it behaves like 4 spaces
 vim.o.undofile = true
 vim.o.ignorecase = true
 vim.o.smartcase = true
-vim.o.signcolumn = 'yes'
+vim.o.signcolumn = "yes"
 vim.o.cursorline = true
-vim.o.inccommand = 'split'
+vim.o.inccommand = "split"
 -- vim.o.confirm = true
